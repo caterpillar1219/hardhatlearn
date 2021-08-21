e130e635953d50209de8a0ff7b89a737f15641dd
ryxusag <ryxusag@gmail.com>
2021-08-21 16:27:44 -0700

    Deployed to Rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,14 @@
 
 require('@nomiclabs/hardhat-ethers');
 
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
+
 module.exports = {
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: { mnemonic: mnemonic },
+    },
+  },
   solidity: "0.8.4",
 };
